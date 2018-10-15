15475255f0332f33b802d4e598e2c751cf06a960
bcp0109 <bcp0109@gmail.com>
2018-10-16 01:25:58 +0900

    gradle commit

--- app/src/main/java/com/explain/MainActivity.java
+++ app/src/main/java/com/explain/MainActivity.java
@@ -2,21 +2,9 @@ package com.explain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
